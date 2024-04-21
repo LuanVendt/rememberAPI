ad87c65d308c814bfe83dcdbbf6c49275b3bb462
LuanVendt <luanvendt@gmail.com>
2024-04-21 15:36:12 -0300

    transactions controller

--- src/resource/transactions/transactions.controller.ts
+++ src/resource/transactions/transactions.controller.ts
@@ -1,34 +1,40 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Query, Put } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { UserEntity } from '../usuarios/entities/user-entity';
+import { query } from 'express';
+import { QueryTransactionDto } from './dto/query-transaction.dto';
 
 @Controller('transactions')
+@UseGuards(JwtAuthGuard)
 export class TransactionsController {
     constructor(private readonly transactionsService: TransactionsService) { }
 
     @Post()
-    create(@Body() createTransactionDto: CreateTransactionDto) {
-        return this.transactionsService.create(createTransactionDto);
+    create(@CurrentUser() currentUser: UserEntity, @Body() createTransactionDto: CreateTransactionDto) {
+        return this.transactionsService.create(String(currentUser.id), createTransactionDto);
     }
 
     @Get()
-    findAll() {
-        return this.transactionsService.findAll();
+    findAll(@CurrentUser() currentUser: UserEntity, @Query() query: QueryTransactionDto) {
+        return this.transactionsService.findAll(String(currentUser.id), query);
     }
 
     @Get(':id')
-    findUnique(@Param('id') id: string) {
-        return this.transactionsService.findUnique(+id);
+    findUnique(@CurrentUser() currentUser: UserEntity, @Param('id') id: string) {
+        return this.transactionsService.findUnique(String(currentUser.id), id);
     }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto) {
-        return this.transactionsService.update(+id, updateTransactionDto);
+    @Put(':id')
+    update(@CurrentUser() currentUser: UserEntity, @Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto) {
+        return this.transactionsService.update(String(currentUser.id), id, updateTransactionDto);
     }
 
     @Delete(':id')
-    delete(@Param('id') id: string) {
-        return this.transactionsService.delete(+id);
+    delete(@CurrentUser() currentUser, @Param('id') id: string) {
+        return this.transactionsService.delete(String(currentUser.id), id);
     }
 }
