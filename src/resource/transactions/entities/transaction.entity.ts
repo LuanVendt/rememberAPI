2c74f57eecd8a30d1c8ab41a2fd46fa384a51fc8
LuanVendt <luanvendt@gmail.com>
2024-04-21 15:15:26 -0300

    task entity

--- src/resource/transactions/entities/transaction.entity.ts
+++ src/resource/transactions/entities/transaction.entity.ts
@@ -1,1 +1,9 @@
-export class Transaction {}
+export class TransactionEntity {
+    id: number;
+    id_usuario: number;
+    descricao: string;
+    preco: number;
+    categoria: string;
+    data: Date;
+    tipo: string;
+}
