5fe34ad178eded741b6831a270301c29e6e2a210
LuanVendt <luanvendt@gmail.com>
2024-04-21 15:13:10 -0300

    query transaction dto

---
+++ src/resource/transactions/dto/query-transaction.dto.ts
@@ -0,0 +1,10 @@
+export class QueryTransactionDto {
+    page?: number;
+    limit?: number;
+    search?: string;
+    descricao?: String;
+    preco?: String;
+    categoria?: String;
+    data?: Date;
+    tipo?: Date;
+}
