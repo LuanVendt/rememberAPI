461cfa63ab4b185e21f49a8231a03c041d86f72e
LuanVendt <luanvendt@gmail.com>
2024-04-21 15:11:13 -0300

    update transaction dto

--- src/resource/transactions/dto/update-transaction.dto.ts
+++ src/resource/transactions/dto/update-transaction.dto.ts
@@ -1,4 +1,25 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTransactionDto } from './create-transaction.dto';
+import { IsDate, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
-export class UpdateTransactionDto extends PartialType(CreateTransactionDto) {}
+export class UpdateTransactionDto extends PartialType(CreateTransactionDto) {
+    @IsOptional({ message: 'id_usuario é obrigatório.' })
+    @IsNumber({}, { message: 'id_usuario precisa ser um número inteiro.' })
+    id_usuario?: number
+
+    @IsOptional({ message: 'descricao é obrigatório' })
+    @IsString({ message: 'descricao precisa ser uma string.' })
+    descricao?: string
+
+    @IsOptional({ message: 'preco é obrigatório' })
+    @IsNumber({}, { message: 'preco precisa ser um número.' })
+    preco?: number
+
+    @IsOptional({ message: 'categoria é obrigatório.' })
+    @IsString({ message: 'categoria precisa ser uma string.' })
+    categoria?: String
+
+    @IsOptional({ message: 'tipo é obrigatório.' })
+    @IsString({ message: 'tipo precisa ser uma string.' })
+    tipo?: string
+}
