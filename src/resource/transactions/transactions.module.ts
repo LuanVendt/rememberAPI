e66f835c24be722902cfbe0b8c18b2eda0242d2b
LuanVendt <luanvendt@gmail.com>
2024-04-21 15:53:28 -0300

    alterando transactions module

--- src/resource/transactions/transactions.module.ts
+++ src/resource/transactions/transactions.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
+import { PrismaService } from 'src/database/PrismaService';
+import { PrismaTransactionsRepository } from './repositories/prisma/prisma-transactions.repository';
+import { TransactionsRepository } from './repositories/transactions.repository';
 
 @Module({
-  controllers: [TransactionsController],
-  providers: [TransactionsService],
+    controllers: [TransactionsController],
+    providers: [
+        PrismaService,
+        TransactionsService,
+        PrismaTransactionsRepository,
+        { provide: TransactionsRepository, useClass: PrismaTransactionsRepository }
+    ],
 })
-export class TransactionsModule {}
+export class TransactionsModule { }
