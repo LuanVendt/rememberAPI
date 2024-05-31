4d292e188b608355f38c77ab2a3c563a2a5f7d6d
LuanVendt <luanvendt@gmail.com>
2024-05-31 15:49:56 -0300

    corrigindo update task

--- src/resource/tasks/dto/update-task.dto.ts
+++ src/resource/tasks/dto/update-task.dto.ts
@@ -1,35 +1,5 @@
 import { IsDate, IsNumber, IsOptional, IsString } from "class-validator";
+import { CreateTaskDto } from "./create-task.dto";
+import { PartialType } from "@nestjs/mapped-types";
 
-export class UpdateTaskDto {
-    @IsNumber({}, { message: "O campo 'id_categoria' deve ser um número inteiro." })
-    @IsOptional()
-    id_categoria?: number;
-
-    @IsNumber({}, { message: "O campo 'id_prioridade' deve ser um número inteiro." })
-    @IsOptional()
-    id_prioridade?: number;
-
-    @IsNumber({}, { message: "O campo 'id_status' deve ser um número inteiro." })
-    @IsOptional()
-    id_status?: number;
-
-    @IsString({ message: "O campo 'nome' deve ser uma string." })
-    @IsOptional()
-    nome?: string;
-
-    @IsString({ message: "O campo 'descricao' deve ser uma string." })
-    @IsOptional()
-    descricao?: string;
-
-    @IsString({ message: "O campo 'string' deve ser uma string." })
-    @IsOptional()
-    anotacao?: string;
-
-    @IsDate({ message: "O campo 'data_criacao' deve ser uma data." })
-    @IsOptional()
-    data_criacao?: Date;
-
-    @IsDate({ message: "O campo 'data_criacao' deve ser uma data." })
-    @IsOptional()
-    data_vencimento?: Date;
-}
+export class UpdateTaskDto extends PartialType(CreateTaskDto) { }
